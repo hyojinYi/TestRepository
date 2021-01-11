c02431a60ee9074fd7c1020432386a8bd68f0260
jane <hyoyi2@naver.com>
2021-01-12 01:09:52 +0900

    first commit

---
+++ TestPjt/TestPjt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestPjt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
