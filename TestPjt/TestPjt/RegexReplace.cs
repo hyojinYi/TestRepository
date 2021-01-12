e2b5c973f745d7c403626d7702d9c98e096214bd
jane <hyoyi2@naver.com>
2021-01-12 16:53:30 +0900

    RegexReplace

---
+++ TestPjt/TestPjt/RegexReplace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TestPjt
+{
+    class RegexReplace
+    {
+        static void Main()
+        {
+            string s = "안녕하세요.     반갑습니다.      또 만나요.";
+            var regex = new Regex("\\s+");
+            string r = regex.Replace(s, " ");
+            Console.WriteLine(s);
+            Console.WriteLine(r);
+        }
+    }
+}
