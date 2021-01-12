ad3fc52f09e72de1a042aedac45221a083ba5e91
jane <hyoyi2@naver.com>
2021-01-12 12:30:13 +0900

    RandomDemo.cs 추가

---
+++ TestPjt/TestPjt/RandomDemo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestPjt
+{
+    class RandomDemo
+    {
+        static void Main()
+        {
+            Random random = new Random();
+            Console.WriteLine(random.Next());
+            Console.WriteLine(random.Next(5));
+            Console.WriteLine(random.Next(1, 10));
+
+        }
+
+    }
+}
