1fcea50571ed8ecf827f5b8d5c5bcabe2ec1743c
jane <hyoyi2@naver.com>
2021-01-12 21:40:52 +0900

    String과 StringBuilder 클래스의 성능 차이 비교

---
+++ TestPjt/TestPjt/StringBuilderPerformance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace TestPjt
+{
+    class StringBuilderPerformance
+    {
+        static void Main()
+        {
+            DateTime start = DateTime.Now;
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+            {
+                sb.Append("안녕하세요.");
+            }
+
+            DateTime end = DateTime.Now;
+            double exec = (end - start).TotalMilliseconds;
+            Console.WriteLine(exec);
+        }
+    }
+}
