468fce45d6451c265300e23d16f61289354201ae
jane <hyoyi2@naver.com>
2021-01-12 18:55:52 +0900

    PatternMatchingWithIf.cs

---
+++ TestPjt/TestPjt/PatternMatchingWithIf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace TestPjt
+{
+    class PatternMatchingWithIf
+    {
+        static void Main()
+        {
+            PrintStars(null);
+            PrintStars("하나");
+            PrintStars(5);
+        }
+        static void PrintStars(object o)
+        {
+            if (o is null)
+            {
+                return;
+            }
+
+            if(o is string)
+            {
+                return;
+            }
+
+            //패턴 매칭: 넘어온 값이 정수 형식이면 int number =o;
+            if (!(o is int number))
+            {
+                return;
+            }
+            WriteLine(new String('*', number));
+        }
+    }
+}
