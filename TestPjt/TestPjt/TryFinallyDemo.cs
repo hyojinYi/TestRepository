8500d3b10c46fc25e64728d0b0ba22a3f8df0af6
jane <hyoyi2@naver.com>
2021-01-13 20:29:54 +0900

    TryFinallyDemo

---
+++ TestPjt/TestPjt/TryFinallyDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestPjt
+{
+    class TryFinallyDemo
+    {
+        static void Main()
+        {
+            Console.WriteLine("[1] 시작");
+
+            try//예외가 발생할 만한 구문이 들어오는 곳
+            {
+                Console.WriteLine("[2] 실행");
+                throw new Exception(); //무작정 에러 발생
+            }
+            finally
+            {
+
+                Console.WriteLine("[3] 종료");
+            }
+        }
+    }
+}
