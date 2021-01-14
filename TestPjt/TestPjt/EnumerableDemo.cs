52f9a90fcdc028a08b2145ff0fa7314f0c0c0cc8
jane <hyoyi2@naver.com>
2021-01-14 10:22:28 +0900

    EnumerableDemo

---
+++ TestPjt/TestPjt/EnumerableDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace TestPjt
+{
+    class EnumerableDemo
+    {
+        static void Main()
+        {
+            var numbers = Enumerable.Range(1, 5);
+            foreach (var n in numbers)
+            {
+                Console.Write($"{n}\t");
+            }
+            Console.WriteLine();
+
+            var sameNumbers = Enumerable.Repeat(-1, 5);
+            foreach (var n in sameNumbers)
+            {
+                Console.Write($"{n}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
