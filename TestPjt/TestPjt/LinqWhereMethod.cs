d37880728a9909fe2b3e9a4c6ec117a5a394b83c
jane <hyoyi2@naver.com>
2021-01-15 11:14:35 +0900

    LinqWhereMethod

---
+++ TestPjt/TestPjt/LinqWhereMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace TestPjt
+{
+    class LinqWhereMethod
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            //배열에서 홀수만 추출 : 람다 식 사용
+            var q = arr.Where(num => num % 2 == 1);
+
+            foreach(var n in q)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
