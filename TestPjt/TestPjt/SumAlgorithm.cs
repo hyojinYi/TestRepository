be960773e8618365a35dd2b6da6e8712189af2bb
jane <hyoyi2@naver.com>
2021-01-15 20:28:26 +0900

    add Linq

--- TestPjt/TestPjt/SumAlgorithm.cs
+++ TestPjt/TestPjt/SumAlgorithm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace TestPjt
 {
@@ -6,17 +7,22 @@ namespace TestPjt
     {
         static void Main()
         {
+            //1
             int[] scores = { 100, 75, 50, 37, 90, 95 };
             int sum = 0;
 
             for (int i = 0; i < scores.Length; i++)
             {
-                if (scores[i] >=80)
+                if (scores[i] >= 80)
                 {
                     sum += scores[i];
                 }
             }
             Console.WriteLine($"{scores.Length}명의 점수 중 80점 이상 총점 : {sum}");
+
+            //2
+            int sum2 = (new int[] { 100, 75, 50, 37, 90, 95 }).Where(s => s >= 80).Sum();
+            Console.WriteLine(sum2);
         }
     }
 }
