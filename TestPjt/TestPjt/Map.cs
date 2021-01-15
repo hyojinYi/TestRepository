df8585aa4642d208a146629e826f4065947ced6c
jane <hyoyi2@naver.com>
2021-01-15 19:01:52 +0900

    Map

---
+++ TestPjt/TestPjt/Map.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace TestPjt
+{
+    class Map
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            var nums = numbers.Select(it => it * it);
+
+            foreach(var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
