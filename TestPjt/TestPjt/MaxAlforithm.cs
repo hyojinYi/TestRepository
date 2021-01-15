88badc2127c70f34e5eb3f78d62040dd27a67173
jane <hyoyi2@naver.com>
2021-01-15 20:52:54 +0900

    MaxAlforithm

---
+++ TestPjt/TestPjt/MaxAlforithm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace TestPjt
+{
+    class MaxAlforithm
+    {
+        static void Main()
+        {
+            int max = int.MinValue;
+
+            int[] numbers = { -2, -5, -3, -7, -1 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+            Console.WriteLine($"최댓값(식) : {numbers.Max()}");
+            Console.WriteLine($"최댓값(문) : {max}");
+        }
+    }
+}
