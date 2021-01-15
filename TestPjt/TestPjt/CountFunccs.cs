cb4f7af2069cb393ee3c7686f3b00ac09956cfe9
jane <hyoyi2@naver.com>
2021-01-15 11:27:15 +0900

    Filters.cs CountFunccs.cs

---
+++ TestPjt/TestPjt/CountFunccs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace TestPjt
+{
+    class CountFunccs
+    {
+        static void Main()
+        {
+            bool[] blns = { true, false, true, false, true };
+
+            Console.WriteLine(blns.Count());
+            Console.WriteLine(blns.Count(bln =>bln == true));
+            Console.WriteLine(blns.Count(bln => bln == false));
+        }
+    }
+}
---
+++ TestPjt/TestPjt/Filtercs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace TestPjt
+{
+    class Filtercs
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            var nums = numbers.Where(it => it % 2 == 0 && it > 3);
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
