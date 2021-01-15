7bb299f595cbf53885132121e0b306ba3876da02
jane <hyoyi2@naver.com>
2021-01-16 08:50:05 +0900

    ObjectNote

---
+++ TestPjt/TestPjt/objectNote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ObjectNote
+{
+    public class Counter
+    {
+        public void GetTodaayVisitCount()
+        {
+            Console.WriteLine("오늘 1234명이 접속했습니다.");
+        }
+    }
+    class ObjectNote
+    {
+        static void Main()
+        {
+            Counter counter = new Counter();
+            counter.GetTodaayVisitCount();
+        }
+    }
+}
