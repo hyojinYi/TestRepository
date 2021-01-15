9f4bbf5ff0aee48c83dd0b2ace6859a09c7e95c8
jane <hyoyi2@naver.com>
2021-01-16 07:34:58 +0900

    ModeAlgorithm

---
+++ TestPjt/TestPjt/ModeAlgorithm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace TestPjt
+{
+    class ModeAlgorithm
+    {
+        static void Main()
+        {
+            int[] scores = { 1, 3, 4, 3, 5 };
+            int[] indexes = new int[5 + 1];
+            int max = int.MinValue;
+            int mode = 0;
+
+            for(int i = 0; i < scores.Length; i++)
+            {
+                indexes[scores[i]]++;
+            }
+            for (int i = 0; i < indexes.Length; i++)
+            {
+                if (indexes[i] > max)
+                {
+                    max = indexes[i];
+                    mode = i;
+                }
+            }
+            Console.WriteLine($"최빈값(문) : {mode} -> {max}번 나타남");
+
+            var q = scores.GroupBy(v => v).OrderByDescending(g => g.Count()).First();
+            int modeCOunt = q.Count();
+            int frequency = q.Key;
+            Console.WriteLine($"최빈값(식) : {frequency} -> {modeCOunt}번 나타남");
+        }
+    }
+}
