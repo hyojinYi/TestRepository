5a806f4013c83da58ce84c4c20e1f954918f1bfa
jane <hyoyi2@naver.com>
2021-01-17 18:09:52 +0900

    ParamsDemo

---
+++ ClassDemo/ClassDemo/DateTimeTryParse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassDemo
+{
+    class DateTimeTryParse
+    {
+        static void Main()
+        {
+            DateTime dt;
+
+            if(DateTime.TryParse("2020-01-01", out dt))
+            {
+                Console.WriteLine(dt);
+            }
+            else
+            {
+                Console.WriteLine("날짜형식으로 변환할 수 없습니다.");
+            }
+            if(DateTime.TryParse("2020-01-01", out var myDate))
+            {
+                Console.WriteLine(myDate);
+            }
+        }
+    }
+}
