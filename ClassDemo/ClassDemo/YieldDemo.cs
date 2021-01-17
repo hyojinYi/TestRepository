0c2b375a7e19ecef98b7ea4afc52086b7e1a49d8
jane <hyoyi2@naver.com>
2021-01-18 00:07:13 +0900

    YieldDemo

---
+++ ClassDemo/ClassDemo/YieldDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace ClassDemo
+{
+    class YieldDemo
+    {
+        static IEnumerable GetNumbers()
+        {
+            yield return 1;
+            yield return 2;
+            for (int i = 3; i <= 5; i++)
+            {
+                yield return i;
+            }
+        }
+
+        static void Main()
+        {
+            foreach (var num in GetNumbers())
+            {
+                Console.WriteLine($"{num}\t", num);
+            }
+            Console.WriteLine();
+        }
+    }
+}
