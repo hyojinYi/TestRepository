a678346de75610b4980011a5650d4cfb4182e4bf
jane <hyoyi2@naver.com>
2021-01-17 23:56:50 +0900

    NickNameDemo

---
+++ ClassDemo/ClassDemo/NickName.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+namespace NickNameDemo
+{
+    public class NickName
+    {
+        private Hashtable _names = new Hashtable();
+
+        public string this[string key]
+        {
+            get { return _names[key].ToString(); }
+            set { _names[key] = value; }
+        }
+    }
+}
