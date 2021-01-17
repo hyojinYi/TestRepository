b8eb2f5caa8147c751cddf3d60ac97391f707d85
jane <hyoyi2@naver.com>
2021-01-17 14:52:15 +0900

    class

---
+++ TestPjt/TestPjt/ConstuctorOverload.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassDemo
+{
+    class ConstructorLog
+    {
+        public ConstructorLog()
+        {
+            Console.WriteLine("기본 생성자 실행");
+        }
+
+        public ConstructorLog(string message)
+        {
+            Console.WriteLine("오버로드된 생성자 실행 : " + message);
+        }
+    }
+    class ConstuctorOverload
+    {
+        static void Main()
+        {
+            ConstructorLog log1 = new ConstructorLog();
+            ConstructorLog log2 = new ConstructorLog("C#");
+            ConstructorLog log3 = new ConstructorLog("ASP.NET");
+        }
+    }
+}
