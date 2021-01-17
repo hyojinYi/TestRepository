2472531ac9f945a3b8f9c109e1717363e04c3732
jane <hyoyi2@naver.com>
2021-01-18 00:02:20 +0900

    yieldReturn

---
+++ ClassDemo/ClassDemo/yieldReturn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace ClassDemo
+{
+    class yieldReturn
+    {
+        static IEnumerable MultiData()
+        {
+            yield return "Hello";
+            yield return "World";
+            yield return "C#";
+        }
+        static void Main()
+        {
+            foreach (var item in MultiData())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
