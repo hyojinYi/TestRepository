c1ec9802ec6ffceb1846bdc0227da5b2e7660d55
jane <hyoyi2@naver.com>
2021-01-17 14:34:03 +0900

    ParameterOut

---
+++ ClassDemo/ClassDemo/ParameterOut.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ClassDemo
+{
+    class ParameterOut
+    {
+        static void Main()
+        {
+            int num;
+
+            Do(out num);
+
+            Console.WriteLine($"[2] {num}");
+        }
+
+        static void Do(out int num)
+        {
+            num = 1234;
+            Console.WriteLine($"[1] {num}");
+        }
+    }
+}
