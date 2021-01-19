4f87cfa433cae93aa3a50da70167a2140901a448
jane <hyoyi2@naver.com>
2021-01-19 11:21:09 +0900

    FuncDelegate

---
+++ ClassDemo/ClassDemo/FuncDelegate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassDemo
+{
+    class FuncDelegate
+    {
+        static void Main()
+        {
+            Console.WriteLine(Add(3, 5));
+
+            Func<int, int, string> AddDelegate = Add;
+            Console.WriteLine(AddDelegate(3, 5));
+
+            Func<int, int, string> AddLamda = (a, b) => (a + b).ToString();
+            Console.WriteLine(AddLamda(3,5));
+        }
+
+        static string Add(int a, int b) => (a + b).ToString();
+    }
+}
