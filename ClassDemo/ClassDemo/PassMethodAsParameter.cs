cc663729ab440ea2a45307d9a4e24b8149b1540c
jane <hyoyi2@naver.com>
2021-01-19 23:15:07 +0900

    PassMethodAsParameter

---
+++ ClassDemo/ClassDemo/PassMethodAsParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassDemo
+{
+    class PassMethodAsParameter
+    {
+        static int StringLength(string data) => data.Length;
+
+        static void StringLengthPrint(Func<string, int> stringLength, string message)
+            => Console.WriteLine($"메시지의 크기는 {stringLength(message)}입니다.");
+
+        static void Main() => StringLengthPrint(StringLength, "안녕하세요 ");
+
+       
+    }
+}
