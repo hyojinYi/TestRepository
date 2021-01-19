00a3b001d5d94ec832188d2cb7586c6ee95d2332
jane <hyoyi2@naver.com>
2021-01-19 23:19:04 +0900

    PassMethodAsParameter

---
+++ ClassDemo/ClassDemo/InheritanceDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassDemo
+{
+    class Parent
+    {
+        public void Foo() => Console.WriteLine("부모 클래스의 멤버 호출");
+    }
+    class Child : Parent
+    {
+        public void Bar() => Console.WriteLine("자식 클래스의 멤버호출");
+    }
+    class InheritanceDemo
+    {
+        static void Main()
+        {
+            var child = new Child();
+            child.Foo();
+            child.Bar();
+        }
+    }
+}
