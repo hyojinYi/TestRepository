380c8997d0f89b39386eb5e23a9a282bff3d173a
jane <hyoyi2@naver.com>
2021-01-21 00:20:07 +0900

    ReflectionGetMethod

---
+++ ClassDemo/ClassDemo/ReflectionGetMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionGetMethod
+{
+    public class MemberClass
+    {
+        public string Name { get; set; } = "길벗출판사";
+        public string GetName()
+        {
+            return Name + ", " + DateTime.Now.ToShortTimeString();
+        }
+    }
+
+    class ReflectionGetMethod
+    {
+        static void Main()
+        {
+            MemberClass m = new MemberClass();
+            Type t = m.GetType();
+
+            PropertyInfo pi = t.GetProperty("Name");
+            Console.WriteLine($"속성 호출 : {pi.GetValue(m)}");
+
+            MethodInfo mi = t.GetMethod("GetName");
+            Console.WriteLine($"메서드 호출 : {mi.Invoke(m, null)}");
+
+            dynamic d = new MemberClass();
+            Console.WriteLine($"속성 호출 : {d.Name}");
+            Console.WriteLine($"메서드 호출 : {d.GetName()}");
+        }
+    }
+}
