f51ff5999b1d3f7330cb20f381eb681f380aded2
jane <hyoyi2@naver.com>
2021-01-22 01:32:36 +0900

    TupleDeconstructionDescription

---
+++ ClassDemo/ClassDemo/TupleDeconstructionDescription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassDemo
+{
+    class TupleDeconstructionDescription
+    {
+        static void Main()
+        {
+            var (sum, count) = Tally();
+            Console.WriteLine($"Sum : {sum}, count ; {count}");
+        }
+        static (int Sum, int Count) Tally()
+        {
+            var r = (s: 12, c: 3);
+            Console.WriteLine($"{r.s}, {r.c}");
+            return r;
+        }
+    }
+}
