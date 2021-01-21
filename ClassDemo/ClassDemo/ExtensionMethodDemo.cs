1a7759926af40528452766de4104075610d1f0fa
jane <hyoyi2@naver.com>
2021-01-21 17:00:41 +0900

    ExtensionMethodDemo

---
+++ ClassDemo/ClassDemo/ExtensionMethodDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExtensionMethodDemo
+{
+    public static class MyClass
+    {
+        public static int WrodCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' },
+                StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+    class ExtensionMethodDemo
+    {
+        static void Main()
+        {
+            string s = "안녕하세요? 확장 메서드... ...";
+            Console.WriteLine(s.Length);
+            Console.WriteLine(s.WrodCount());
+        }
+    }
+}
