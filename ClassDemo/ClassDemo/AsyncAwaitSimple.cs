29520611d7271fb0d1a67b137555c2334d35337a
jane <hyoyi2@naver.com>
2021-01-23 01:11:27 +0900

    AsyncAwaitSimple

---
+++ ClassDemo/ClassDemo/AsyncAwaitSimple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class AsyncAwaitSimple
+    {
+        static async Task DoAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var r = await client.GetAsync("https://www.kosaf.go.kr/ko/main.do");
+                var c = await r.Content.ReadAsStringAsync();
+
+                Console.WriteLine(c); 
+            }
+        }
+
+        static async Task Main()
+        {
+            await DoAsync();
+        }
+    }
+}
