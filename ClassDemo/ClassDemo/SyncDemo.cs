0c9e037353f2b971ebc3b4eab8d0c861e1792a06
jane <hyoyi2@naver.com>
2021-01-23 01:02:16 +0900

    SyncDemo

---
+++ ClassDemo/ClassDemo/SyncDemo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassDemo
+{
+    class SyncDemo
+    {
+        static void Sum(int a, int b) => Console.WriteLine($"{a} + {b} = {a + b}");
+        static void Main()
+        {
+            Sum(3, 5);
+            Sum(5, 7);
+            Sum(7, 9);
+        }
+    }
+}
