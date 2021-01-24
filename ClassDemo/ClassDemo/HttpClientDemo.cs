d097dda4e6938c81c6a796f7ec00b8ae57b341bf
jane <hyoyi2@naver.com>
2021-01-24 20:20:03 +0900

    HttpClientDemo

---
+++ ClassDemo/ClassDemo/HttpClientDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class HttpClientDemo
+    {
+        static async Task Main()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync("http://www.microsoft.com/");
+
+            string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+
+            Console.WriteLine(responseBody);
+        }
+    }
+}
