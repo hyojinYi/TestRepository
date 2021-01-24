548023d8848d96e26fc2acf1fe73556214ae4fee
jane <hyoyi2@naver.com>
2021-01-24 22:23:02 +0900

    PatternMathchingIf

---
+++ ClassDemo/ClassDemo/PatternMathchingIf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PatternMathchingIf
+{
+    class Shape { }
+
+    class Rectangle : Shape
+    {
+        public string Name { get; set; } = "사각형";
+    }
+
+    class PatternMathchingIf
+    {
+        static void Main() => ShowShape(new Rectangle());
+
+        static void ShowShape(Shape shape)
+        {
+            if(shape is Rectangle r)
+            {
+                Console.WriteLine(r.Name);
+            }
+        }
+    }
+}
